08a1a4afe8a6dc6528e6f505c1a6299acab29bef
akit <149289426+Sstudiosdev@users.noreply.github.com>
2024-06-17 16:30:12 -0600

    Improved insole design

--- app/theme/azure-dark.tcl
+++ app/theme/azure-dark.tcl
@@ -5,11 +5,11 @@ namespace eval ::ttk::theme::azure-dark {
 
     proc Theme {} {
         ttk::style theme create azure-dark -parent default -settings {
-            ttk::style configure TButton -background #2e2e2e -foreground #ffffff -font {Arial 10}
-            ttk::style configure TLabel -background #2e2e2e -foreground #ffffff -font {Arial 10}
-            ttk::style configure TEntry -background #2e2e2e -foreground #ffffff -font {Arial 10}
-            ttk::style configure TText -background #2e2e2e -foreground #ffffff -font {Arial 10}
-            ttk::style configure TFrame -background #2e2e2e -foreground #ffffff
+            ttk::style configure TButton -background #3498db -foreground #ffffff -font {Arial 10} -padding {5 10}
+            ttk::style configure TLabel -foreground #333333 -font {Arial 10 bold}
+            ttk::style configure TEntry -background #ecf0f1 -foreground #333333 -font {Arial 10} -padding 5
+            ttk::style configure TText -background #ecf0f1 -foreground #333333 -font {Arial 10} -padding 5
+            ttk::style configure TFrame -background #ecf0f1
         }
     }
 }
