2cbddff232a37ae9dd1e856a7339e9a6c6e4c1d4
akit <149289426+Sstudiosdev@users.noreply.github.com>
2024-06-19 16:34:43 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
+# Import the run_app function from the app.ui module
 from app.ui import run_app
 
+# Check if this module is being run as the main program
 if __name__ == "__main__":
+    # Execute the run_app function to start the application
     run_app()
