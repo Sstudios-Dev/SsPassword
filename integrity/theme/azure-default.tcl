2add2188f5bf2f74b0913f75e9dad56789989c92
akit <149289426+Sstudiosdev@users.noreply.github.com>
2024-06-20 13:39:50 -0600

    Update azure-default.tcl

--- integrity/theme/azure-default.tcl
+++ integrity/theme/azure-default.tcl
@@ -5,14 +5,12 @@ namespace eval ::ttk::theme::azure-default {
 
     proc Theme {} {
         ttk::style theme create azure-default -parent default -settings {
-            ttk::style configure TButton -background #3498db -foreground #ffffff -font {Arial 10} -padding {5 10}
-            ttk::style configure TLabel -foreground #333333 -font {Arial 10 bold}
-            ttk::style configure TEntry -background #ecf0f1 -foreground #333333 -font {Arial 10} -padding 5
-            ttk::style configure TText -background #ecf0f1 -foreground #333333 -font {Arial 10} -padding 5
-            ttk::style configure TFrame -background #ecf0f1
-
-            # Style for when the mouse is over the button
+            ttk::style configure TButton -background #3498db -foreground #ffffff -font {Arial 10 bold} -padding {8 12} -borderwidth 2 -relief groove
             ttk::style map TButton -background [list active #2980b9]
+            ttk::style configure TLabel -foreground #333333 -font {Arial 10 bold}
+            ttk::style configure TEntry -background #ecf0f1 -foreground #333333 -font {Arial 10} -padding 5 -borderwidth 1 -relief solid
+            ttk::style configure TText -background #ecf0f1 -foreground #333333 -font {Arial 10} -padding 5 -borderwidth 1 -relief solid
+            ttk::style configure TFrame -background #ecf0f1 -borderwidth 0
         }
     }
 }
